0521cbe9cf5d212e58cd8352ae492f3a017e23fb
Jahnavikattoju2223 <147685310+Jahnavikattoju2223@users.noreply.github.com>
2024-01-12 10:30:42 +0530

    Create sunny.py

---
+++ sunny.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.header('sunny')
+st.write('sunny')
+st.image('https://1.bp.blogspot.com/-d_Hc6lyiYpU/V1GxIBQxSSI/AAAAAAAAAvQ/Nv1tZOc91eUJvY3FEwiPBeGi1o-wR_BAwCLcB/s1600/sunrise.jpg',caption = 'sunny')
