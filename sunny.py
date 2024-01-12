cf0cf59617f1ff606ac2e00a19c52198a22ffb74
Jahnavikattoju2223 <147685310+Jahnavikattoju2223@users.noreply.github.com>
2024-01-12 10:44:14 +0530

    Update sunny.py

--- sunny.py
+++ sunny.py
@@ -1,4 +1,4 @@
 import streamlit as st
 st.header('sunny')
 st.write('sunny')
-st.video('https://youtu.be/TRXYkuQqamQ?t=7',caption = 'sunny')
+st.video('https://youtu.be/Ldn11dMHTJ8?si=kmDAPaJK6SXR1Xn0')
