1a0eec7db83e17aa0dde1c2d530a6ccc82152d9d
Jahnavikattoju2223 <147685310+Jahnavikattoju2223@users.noreply.github.com>
2024-01-12 11:11:02 +0530

    Update sunny.py

--- sunny.py
+++ sunny.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.header('odiyamma')
+st.header('Druv..')
 st.image('https://i.pinimg.com/originals/83/59/96/8359967dfe0b0dd88d61b05210c58115.jpg')
-st.header('Druv...')
+st.header('Odiyamma...')
 st.video('https://youtu.be/ppsfE42ZSzU?si=2OFVkFh2bNSKSzA7')
