694eb6612c04f038ba160db33c83cf4ef48cc994
Jahnavikattoju2223 <147685310+Jahnavikattoju2223@users.noreply.github.com>
2024-01-12 11:10:05 +0530

    Update sunny.py

--- sunny.py
+++ sunny.py
@@ -2,4 +2,4 @@ import streamlit as st
 st.header('odiyamma')
 st.image('https://i.pinimg.com/originals/83/59/96/8359967dfe0b0dd88d61b05210c58115.jpg')
 st.header('Druv...')
-st.video('https://youtube.com/shorts/cmwpHsqoVVc?si=ypwyDDN_iGYWSgVp')
+st.video('https://youtu.be/ppsfE42ZSzU?si=2OFVkFh2bNSKSzA7')
